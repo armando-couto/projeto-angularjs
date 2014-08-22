5e5149c2d76ab4f2820522d7b37b2bbd6c395afe
armandocouto <coutoarmando@gmail.com>
2014-08-22 13:34:56 -0300

    Modificações.

---
+++ client/js/clientesController.js
@@ -0,0 +1,61 @@
+
+
+function clientesController($scope,$http,$routeParams,$location)
+{
+	//lista de clientes
+	$scope.rows = null;
+
+	//um cliente 
+	$scope.row = null;
+
+	//Pagination
+	$scope.currentPage = 0;
+	$scope.pageSize = 15;
+
+	$scope.numberOfPages =function(){
+		return Math.ceil($scope.rows.length/$scope.pageSize);                
+	}
+
+	$scope.loadAll = function(){
+		$scope.showLoader();
+		$http.get($scope.server("/customers")).success(function(data){
+			$scope.rows = data;			
+		});
+	}
+
+	$scope.loadRow = function(){
+		if ($routeParams.id!=null)
+		{
+			$scope.showLoader();
+			$http.get($scope.server("/customer/"+$routeParams.id)).success(function(data){
+				$scope.row = data;
+				$scope.row.isUpdate = true;
+			});
+		}
+		else
+		{
+			$scope.row = {}
+			$scope.row.CustomerID = null;
+			$scope.row.isUpdate = false;
+		}
+	}
+
+	$scope.save = function(){
+		$scope.showLoader();
+		$http.post($scope.server("/customer/"+$routeParams.id),$scope.row).success(function(data){
+			alert("Salvo com sucesso");
+			$scope.row.isUpdate = true;
+		});
+	}
+
+	$scope.del = function(){
+		if (confirm("Deseja excluir " + $scope.row.CustomerID + "?")){
+			$http.delete($scope.server("/customer/"+$routeParams.id)).success(function(s){
+				alert("Excluído com sucesso");
+				$location.path("/clientes");
+			});
+		}
+
+	}
+
+}
---
+++ restfull-slim/index.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+require 'config.php';
+require 'DB.php';
+
+require 'Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
+$app = new \Slim\Slim(array('debug' => false));
+
+$app->contentType("application/json");
+
+$app->error(function ( Exception $e = null) use ($app) {
+	echo '{"error":{"text":"'. $e->getMessage() .'"}}';
+});
+
+function formatJson($obj){
+	echo json_encode($obj);
+}
+
+//Includes
+include("customer.php");
+
+$app->run();
