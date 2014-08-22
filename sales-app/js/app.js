08ecc22399f25ba135da89dbc10e71b93b744a48
armandocouto <coutoarmando@gmail.com>
2014-08-22 13:44:44 -0300

    Concluíndo configuração com  xampp.

--- sales-app/js/app.js
+++ sales-app/js/app.js
@@ -1,5 +1,5 @@
 //URL de acesso ao servidor RESTful
-SERVER_URL = "http://localhost/sales-server";
+SERVER_URL = "http://localhost/projeto-angularjs/sales-server";
 
 //Criação ao $app que é o modulo que representa toda a aplicação
 $app = angular.module('app',[]);
